6bdbee80f20c4c81d468fa85f06ca9dd4f5b668e
Alan-AC7 <xxprolegendzxx@gmail.com>
2023-12-01 16:03:18 -0600

    Styled

--- src/App.jsx
+++ src/App.jsx
@@ -45,53 +45,58 @@ export default function App() {
 
   return (
     <main>
-      <h1>Bitcoin Connect Donation Modal Demo</h1>
+      <h1 class= "font-bold">Bitcoin Connect Donation Modal Demo</h1>
       <p>
         This client side web application will request permission to donate to a lightning adrress using Bitcoin Connect.
       </p>
       <br></br>
       <Modal />
-      <form>
-        <label>
-          Lightning Address:
-          <input
-            type="text"
-            name="address"
-            value={donationData.address}
-            onChange={handleInputChange}
-          />
-          <br></br>
-        </label>
-        <br />
-        <label>
-          Amount (sats):
-          <input
-            type="number"
-            name="amount"
-            value={donationData.amount}
-            onChange={handleInputChange}
-          />
-          <br></br>
-        </label>
-        <br />
-        <button type="button" onClick={handleDonation}>
-          Send Donation
-        </button>
-      </form>
-      {connected && (
-        <button
-          onClick={() => {
-            disconnect();
-            setConnected(false);
-          }}
-        >
-          Disconnect
-        </button>
-      )}
+      <form class="max-w-sm mx-auto border-4 border-[rgb(25,108,233)] p-4 rounded-lg grid gap-4 md:flex-1 md:max-w-md my-2 md:my-8 lg:my-10 bg-white dark:bg-neutral-800 w-full">
+  <label>
+    Lightning Address:
+    <br></br>
+    </label>
+    <input
+      type="text"
+      name="address"
+      value={donationData.address}
+      onChange={handleInputChange}
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    />
+  <label class='m'>
+    <br></br>
+    Amount (sats):
+    </label>
+    <input
+      type="number"
+      name="amount"
+      value={donationData.amount}
+      onChange={handleInputChange}
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    />
+  
+  <br></br>
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleDonation}>
+    Send Donation
+  </button>
+  {connected && (
+    <button
+      onClick={() => {
+        disconnect();
+        setConnected(false);
+      }}
+      class="text-white bg-[rgb(25,108,233)] text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-4 py-2"
+    >
+      Disconnect
+    </button>
+  )}
+</form>
+
+      <br></br>
+     
       <hr />
-      <a href="https://replit.com/@rolznz/Bitcoin-Connect-Request-Payment-Modal#src/App.jsx">
-        Source code on replit
-      </a>
+      <br></br>
+     
     </main>
   );
 }
